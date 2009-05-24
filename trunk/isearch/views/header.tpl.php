33f6aad53254859aef33e144caacc0fb54efde5d
imehesz <imehesz@12ce76cc-4188-11de-af2b-f15df6ab9c44>
2009-05-24 01:27:23 +0000

    removed sample stuff, added some comments to the code and finished the search feature

--- trunk/isearch/views/header.tpl.php
+++ trunk/isearch/views/header.tpl.php
@@ -3,15 +3,10 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>iUI Music Demo</title>
+<title>iSearch demo</title>
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
 <link rel="apple-touch-icon" href="iui/iui-logo-touch-icon.png" />
 <meta name="apple-touch-fullscreen" content="YES" />
 <style type="text/css" media="screen">@import "public/css/iui.css";</style>
 <script type="application/x-javascript" src="public/js/iui.js"></script>
-<!--
-<script type="application/x-javascript" src="http://10.0.1.2:1840/ibug.js"></script>
--->
 </head>
-
-
