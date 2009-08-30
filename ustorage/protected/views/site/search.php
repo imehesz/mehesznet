474b3c9b16f71024dbdc337bc778a0407561a9fe
imehesz <imehesz@12ce76cc-4188-11de-af2b-f15df6ab9c44>
2009-08-30 03:06:38 +0000

    added and implemented an IMDB library into Yii and started the basic search controller/action

---
+++ ustorage/protected/views/site/search.php
@@ -0,0 +1,94 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<!-- top -->
+		<div id="top">
+			<div>
+                <a href="#" class="tag tag1">4g communications</a>
+                <a href="#" class="tag tag2">telecom jobs</a>
+                <a href="#" class="tag tag3">RF design</a>
+                <a href="#" class="tag tag4">LTE</a>
+                <a href="#" class="tag tag5">3g ringtones</a>
+            </div>
+
+			<div>
+                <a href="#" class="tag tag6">BREW</a>
+                <a href="#" class="tag tag7">UMTS networks</a>
+                <a href="#" class="tag tag8">Bluetooth gps</a>
+                <a href="#" class="tag tag9">GIS</a>
+                <a href="#" class="tag tag10">broadband internet</a>
+            </div>
+
+			<div>
+                <a href="#" class="tag tag11">gsm mobile</a>
+                <a href="/" class="logo"><img src="images/storedbyu_255x99.png" alt="stored.by.u" title="stored.by.u" /></a>
+                <a href="#" class="tag tag12">WCDMA </a>
+            </div>
+
+			<div>
+                <a href="#" class="tag tag13">j2me games</a>
+                <a href="#" class="tag tag14">HSDPA games</a>
+                <a href="#" class="tag tag15">Zigbee products</a>
+                <a href="#" class="tag tag16">live video</a>
+                <a href="#" class="tag tag17">wimax equipment </a>
+            </div>
+
+	  </div>
+
+<!-- search and menus -->
+
+		<div id="sm">
+
+<!-- main menu -->
+<?php /*
+			<div id="menu">
+				<a href="Hardware_Vendors_Equipment_Manufacturers">Equipment</a>
+				<a href="Mobile_Applications_Software_Development">Applications</a>
+				<a href="Wireless_Telecom_Services_Resources">Resources</a>
+				<a href="Operators_Carriers_Service_Providers">Operators</a>
+				<a href="/Agencies_Organizations_Regulatory">Agencies</a>
+
+				<a href="/Telecommunications_Distribution">Distribution</a>
+
+
+				<a href="#" class="more">more</a>
+
+				<div class="moredisplay">
+					<a href="http://www.3gwirelessjobs.com">Jobs &amp; Employment</a>
+					<a href="Products/Mobile_Cell_Phones_Accessories">Mobile Phones</a>
+					<a href="http://forum.3gwirelessjobs.com" >Forums</a>
+				    <a href="Products">Products</a>
+				    <a href="/news/">News</a>
+
+					<a href="/facts/">Interesting</a>
+								</div>
+			</div>
+
+ */ ?>
+<!-- search -->
+			<div id="search">
+						<!-- Google CSE Search Box Begins  -->
+						<form action="?r=site/list" method="post">
+						<fieldset>
+						  <input type="text" name="q" id="searchField"  size="30" value="SEARCH HERE ..." onClick="javascript:this.value='';" onBlur="javascript:if(this.value.length==0){this.value='SEARCH HERE ...';}" />
+						  <div class="b">
+								<input  type="image" src="/images/search.gif"/>
+						  </div>
+						  </fieldset>
+						</form>
+				<p>searching only in Movies ... for now</p>
+			</div>
+
+<!-- browse by -->
+            <?php /*
+			<div id="browse">
+				browse by: <a href="/Technology">Technology</a> <a href="/show">Country</a> <a href="/show/metroarealist">Metro area</a> <a href="/show/letter/">Alphabetical</a>
+
+			</div>
+            */ ?>
+		</div>
---
+++ ustorage/tmp/imdb/test/index.php
@@ -0,0 +1,54 @@
+<?php
+ #############################################################################
+ # IMDBPHP                              (c) Giorgos Giagas & Itzchak Rehberg #
+ # written by Giorgos Giagas                                                 #
+ # extended & maintained by Itzchak Rehberg <izzysoft AT qumran DOT org>     #
+ # http://www.izzysoft.de/                                                   #
+ # ------------------------------------------------------------------------- #
+ # This program is free software; you can redistribute and/or modify it      #
+ # under the terms of the GNU General Public License (see doc/LICENSE)       #
+ #############################################################################
+
+ /* $Id: index.php 175 2008-10-06 14:09:21Z izzy $ */
+
+#====================================================[ Output HTML Header ]===
+echo "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>\n";
+echo "<HTML><HEAD>\n"
+   . " <TITLE>IMDBAPI Checks</TITLE>\n"
+   . " <STYLE TYPE='text/css'>body,td,th { font-size:12px; font-family:sans-serif; }</STYLE>\n"
+   . "</HEAD><BODY>\n";
+
+#==========================================[ Define what should be tested ]===
+$check_movie   = false;
+$check_name    = false;
+$check_charts  = false;
+$check_nowplay = false;
+$check_trail   = false;
+switch ($_REQUEST["check"]) {
+  case "movie"   : $check_movie   = true; break;
+  case "name"    : $check_name    = true; break;
+  case "charts"  : $check_charts  = true; break;
+  case "nowplay" : $check_nowplay = true; break;
+  case "trail"   : $check_trail   = true; break;
+  default:
+    $check_movie   = true;
+    $check_name    = true;
+    $check_charts  = true;
+    $check_nowplay = true;
+    $check_trail   = true;
+}
+
+#=========================================================[ Run the tests ]===
+if ($check_movie)   require ("imdb.inc");
+if ($check_name)    require ("imdb_person.inc");
+if ($check_charts)  require ("imdb_charts.inc");
+if ($check_nowplay) require ("imdb_nowplaying.inc");
+if ($check_trail)   require ("imdb_trailers.inc");
+
+#===============================================[ Summary and HTML footer ]===
+$passed  = $methods - $failures;
+$percent = round(100*$passed/$methods)."%";
+echo "<H3>Test Results:</H3><UL><LI>Methods: $methods</LI><LI>Passed: $passed</LI><LI>Failures: $failures</LI><LI>Success: $percent</LI></UL>";
+if ($percent == "100%") echo "<b>Congratulations!</b> Looks like the complete API is working perfectly.";
+echo "</BODY></HTML>\n";
+?>
